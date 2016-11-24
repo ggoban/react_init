2078696505799b61f86a7414e72f89c2ee5bb322
ggoban <ifmklif@gmail.com>
2016-11-24 10:14:51 +0900

    remove semicolone

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from 'react'
 
 class App extends React.Component {
   render() {
     return (
       <h1>Hello React Skeleton</h1>
-    );
+    )
   }
 }
 
-export default App;
+export default App
